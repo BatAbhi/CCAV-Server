ddd9f2dad65bed4b606da9a964bc0c6841a5fee2
nileshtarte <nilesh@blitzcodesys.com>
2018-11-23 18:50:10 +0530

    Second commit

--- api/controllers/payment/index.js
+++ api/controllers/payment/index.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const router = express.Router()
-const ccav = require('./../../config/ccavutil')
+const express = require("express");
+const router = express.Router();
+const ccav = require("./../../config/ccavutil");
 
+router.post("/post", (req, res, next) => {
+  var body = req.body;
 
-router.post('/post', (req, res, next) => {
+  var obj = "";
 
-    var body = JSON.stringify(req.body);
-    var workingKey = 'XXXXXXXXXXXXXXX'
-    var encRequest = ccav.encrypt(body,workingKey); 
-    try {
-        console.log(body);
-        res.json(encRequest);
-    }
-    catch (err) {
-      console.log(err)
-    }
+  for (var key in body) {
+    //formData.append(key, body[key]);
+    obj=obj+key+"="+body[key]+"&";
 
+  }
+
+  var workingKey = "XXX";
+  var encRequest = ccav.encrypt(obj, workingKey);
+  try {
+    console.log(body);
+    res.json(encRequest);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-
-
-
-module.exports = router
+module.exports = router;
